5d0240d9805ec8b96e6408d55a35c60882d1fb27
Ramsey <someramseyy@gmail.com>
2024-08-17 09:20:24 +0200

    Tested some solutions and added gitignore

--- Program.cs
+++ Program.cs
@@ -1,1 +1,43 @@
-﻿
+﻿using System.Net;
+using PacketDotNet;
+using SharpPcap;
+using SharpPcap.LibPcap;
+
+
+var deviceList = LibPcapLiveDeviceList.Instance;
+var devicesBeingCaptured = new List<LibPcapLiveDevice>();
+var hostMap = new Dictionary<string, string?>();
+
+
+StartCaptureDevice(deviceList[4]);
+
+Console.WriteLine("Press Enter to stop capturing...");
+Console.ReadLine();
+
+foreach (var device in devicesBeingCaptured) {
+    StopCaptureDevice(device);
+}
+
+void DeviceOnPacketArrival(object sender, PacketCapture captureEvent) {
+    var rawPacket = captureEvent.GetPacket();
+    var packet = Packet.ParsePacket(rawPacket.LinkLayerType, rawPacket.Data);
+    var ipPacket = packet.Extract<IPPacket>();
+
+    if (ipPacket != null) {
+        Console.WriteLine($"Packet from {ipPacket.SourceAddress} to {ipPacket.DestinationAddress}");
+    }
+}
+
+void StartCaptureDevice(LibPcapLiveDevice device) {
+    Console.WriteLine($"Capturing '{device.Description}'");
+
+    device.OnPacketArrival += DeviceOnPacketArrival;
+    device.Open(DeviceModes.Promiscuous);
+    device.StartCapture();
+    devicesBeingCaptured.Add(device);
+}
+
+void StopCaptureDevice(LibPcapLiveDevice device) {
+    device.StopCapture();
+    device.Close();
+}
